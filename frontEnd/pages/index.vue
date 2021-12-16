b11dec4935149146008ec6e43f94e0ce0c742040
Edmar-Sousa <Edmar_Sousa>
2021-12-15 22:12:16 -0200

    refectory all files

--- frontEnd/pages/index.vue
+++ frontEnd/pages/index.vue
@@ -1,14 +1,5 @@
 <template>
-    <div class="page-container" v-bind:class="{ 'dark' : pageLightTheme }">
-        <header-component v-on:changeTheme="changeTheme" />
-        <search-component v-bind:theme="pageLightTheme" />
-
-        <div class="job-container">
-            <card-component v-bind:theme="pageLightTheme" />
-            <card-component v-bind:theme="pageLightTheme" />
-            <card-component v-bind:theme="pageLightTheme" />
-        </div>
-
+    <div>
     </div>
 </template>
 
@@ -17,56 +8,10 @@ import Vue from 'vue'
 
 export default Vue.extend({
     name : 'index',
-
-    data() {
-        return {
-            pageLightTheme : true
-        }
-    },
-
-    methods : {
-        changeTheme : function (lightTheme : boolean) {
-            this.pageLightTheme = lightTheme
-        }
-    }
 })
 </script>
 
 
 <style>
-
-div.page-container {
-    width: 100vw;
-    min-height: 100vh;
-
-    transition: background 400ms;
-
-    background: var(--background-light-color);
-    color: var(--font-dark-color);
-}
-
-div.page-container.dark {
-    background: var(--background-dark-color);
-    color: var(--font-light-color);
-}
-
-div.job-container {
-    max-width: 1200px;
-    width: 100%;
-
-    margin: 30px auto;
-    padding: 30px 10px;
-
-    display: flex;
-    flex-wrap: wrap;
-}
-
-@media (max-width: 720px) {
-    div.job-container {
-        display: flex;
-        flex-direction: column;
-    }
-}
-
 
 </style>
