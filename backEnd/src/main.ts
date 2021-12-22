27738e85ea5476ab75cdbba1cd674b2018c37727
Edmar-Sousa <edmarsousamendes4@gmail.com>
2021-12-22 21:13:17 -0200

    add cors in API

--- backEnd/src/main.ts
+++ backEnd/src/main.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import routes  from './routes'
+import cors    from 'cors'
 
 import './database/connection'
 
 
 const app  = express()
 const PORT = 8000 || process.env.PORT
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
